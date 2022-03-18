5752e731a8cf4dca839ad0508bfa4af8ebc96d22
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2022-03-18 15:13:33 +0200

    Flipping cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,20 +2,22 @@ import React from "react";
 
 import "./Card.css";
 
-export const Card = ({ card, choiceHandler }) => {
+export const Card = ({ card, choiceHandler, flipped }) => {
   const clickCardHandler = () => {
     choiceHandler(card);
   };
 
   return (
     <div className="card__grid--item">
-      <img className="card__grid--front" src={card.src} alt="card front" />
-      <img
-        className="card__grid--back"
-        src="/img/cover.png"
-        onClick={clickCardHandler}
-        alt="card front"
-      />
+      <div className={flipped ? "flipped" : ""}>
+        <img className="card__grid--front" src={card.src} alt="card front" />
+        <img
+          className="card__grid--back"
+          src="/img/cover.png"
+          onClick={clickCardHandler}
+          alt="card back"
+        />
+      </div>
     </div>
   );
 };
